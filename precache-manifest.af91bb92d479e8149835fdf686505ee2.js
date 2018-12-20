0d516bbe0f1bfa5c61d9e73e6aaa5ea87cf5e497
tblane <tblane88@gmail.com>
2018-12-19 23:48:11 -0500

    Updates

---
+++ precache-manifest.af91bb92d479e8149835fdf686505ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "186852da5e8854e8ddc3",
+    "url": "/theclickgame/static/js/runtime~main.186852da.js"
+  },
+  {
+    "revision": "b1af68d57608ae3dc1d5",
+    "url": "/theclickgame/static/js/main.b1af68d5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/theclickgame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b1af68d57608ae3dc1d5",
+    "url": "/theclickgame/static/css/main.0a79037d.chunk.css"
+  },
+  {
+    "revision": "934fe0a9840e43ac0ee8a12bfc593a0a",
+    "url": "/theclickgame/index.html"
+  }
+];
